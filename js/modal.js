d73e42bbfe13106c9d3451af9f4d8a31f8285d5d
Unknown <zack@zruybal.com>
2016-11-04 09:54:56 -0600

    Started work on regular modal
    And then realized half way through I'm doing it wrong. Oh well.

---
+++ js/modal.js
@@ -0,0 +1,52 @@
+(function($) {
+    $.fn.modal = funtion(opt) {
+        var settings, createModal, closeModal, body;
+
+        settings = $.extend({
+            'modal': 'modal-window',
+            'close': 'modal-close',
+            'closeText': 'x',
+            'shade': 'modal-shade'
+        }, opt);
+
+        body = $('body');
+
+        closeModal = function(modal, shade){
+            modal.remove();
+            shade.remove();
+        };
+
+        createModal = function(data) {
+                var shade, close, modal;
+
+                shade = $('<div />', {
+                    class: settings.shade
+                }).on('click', function(e){
+                        closeModal(modal,shade);
+                });
+
+                close = $('<a />', {
+                    text: settings.closeText,
+                    class: settings.close,
+                    href: '#'
+                }).on('click', function(e){
+                    closeModal(modal, shade);
+                    e.preventDefault();
+                });
+
+                modal = $('<div />', {
+                    html: data,
+                    class: settings.modal
+                }).append(close);
+
+                body.prepend(shade, modal);
+        };
+
+        this.on('click', function(e){
+            var self = $(this);
+
+            $.
+        })
+
+    }
+})(jQuery);
