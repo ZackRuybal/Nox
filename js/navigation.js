d58f9bdfa95746492edf9dac54dc2bf568f9f0ab
Zack <zack@zruybal.com>
2016-12-04 16:36:54 -0700

    Updated Nav JS
    Updated the navigation.js file so that it doesn't require you to add extra markup than you need to. Now, the script pulls the nav content from the .nav-item class and dumps it into a new element, and then toggles from there.

--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,28 @@
+$( document ).ready(function() {
+	$('body').append('<div class="nav-side"></div>');
+	$navElem = $('.nav-side');
+	$navItems = $('.nav .res-menu > ul');
+	$navItems.last().clone({
+		withDataAndEvents: true
+	}).appendTo($navElem);
+});
 $('.nav .res-only').click(function() {
+
+
 	$toggle = $('.res-only');
-	$content = $('.nav-side');
-	$content.offset({ top: 50 });
-	$content.slideToggle(500, function(){
-		$content.offset({ top: 50 });
+	$appTo = $('.nav-side');
+	$appTo.slideToggle(500, function() {
 		$toggle.text(function() {
-			return $content.is(":visible") ? "Hide" : "Show";
+			return $appTo.is(":visible") ? "Hide" : "Show";
 		});
 	});
+
+	//$toggle = $('.res-only');
+	//$content = $('.nav .res-menu > ul');
+	//$appTo = $('.nav-side');
+
+	$appTo.toggleClass('navIsVisible');
+
+	//$($content).appendTo($appTo);
+	//$('.nav-side').css({"display": "block", "top": "50px"});
 });
