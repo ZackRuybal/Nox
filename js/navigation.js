fd0a45a65f8260501423f8fea931103c7efe58a8
Zack <zack@zruybal.com>
2017-04-17 21:25:40 -0600

    Lots of changes
    Commented out navigation, added images for the landing page (changing them up), added Sketch source files for them as well. They are not final.

--- js/navigation.js
+++ js/navigation.js
@@ -1,31 +1,34 @@
 // (c) 2017 Zack Ruybal. All rights reserved
+// Cleaned comments in April 17.
 $( document ).ready(function() {
+	// Adds the outline for the nav
 	$('body').append('<div class="nav-side"></div>');
+	
+	// Selects what the side nav class is
 	$navElem = $('.nav-side');
+	
+	// Selects the list
 	$navItems = $('.nav .res-menu > ul');
+	
+	// Clones the content of the regular nav
 	$navItems.last().clone({
 		withDataAndEvents: true
 	}).appendTo($navElem);
+	
+	// Appends the bars to the responsive nav
 	$('.res-only').append('<span class="fa fa-bars"></span>');
 });
 $('.nav .res-only').click(function() {
 
-
+	// Toggles the nav
 	$toggle = $('.res-only');
+	
+	// What to append to
 	$appTo = $('.nav-side');
+	
+	// The animation
 	$appTo.slideToggle(500);
-	/*, function() {
-		$toggle.text(function() {
-			return $appTo.is(":visible") ? "Hide" : "Show";
-		});
-	});*/
 
-	//$toggle = $('.res-only');
-	//$content = $('.nav .res-menu > ul');
-	//$appTo = $('.nav-side');
-
+	// Toggles the class (obviously)
 	$appTo.toggleClass('navIsVisible');
-
-	//$($content).appendTo($appTo);
-	//$('.nav-side').css({"display": "block", "top": "50px"});
 });
