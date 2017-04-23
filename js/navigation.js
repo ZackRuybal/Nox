b185bdc72b8c15ee40a22fffaf0c19b9b0a7b5e4
Zack Ruybal <zack@zruybal.com>
2017-04-22 18:47:30 -0600

    Added prevent default
    Why would we want to jump to the top?

--- js/navigation.js
+++ js/navigation.js
@@ -3,18 +3,18 @@
 $( document ).ready(function() {
 	// Adds the outline for the nav
 	$('body').append('<div class="nav-side"></div>');
-	
+
 	// Selects what the side nav class is
 	$navElem = $('.nav-side');
-	
+
 	// Selects the list
 	$navItems = $('.nav .res-menu > ul');
-	
+
 	// Clones the content of the regular nav
 	$navItems.last().clone({
 		withDataAndEvents: true
 	}).appendTo($navElem);
-	
+
 	// Appends the bars to the responsive nav
 	$('.res-only').append('<span class="fa fa-bars"></span>');
 });
@@ -22,13 +22,13 @@ $('.nav .res-only').click(function() {
 
 	// Toggles the nav
 	$toggle = $('.res-only');
-	
+
 	// What to append to
 	$appTo = $('.nav-side');
-	
+
 	// The animation
 	$appTo.slideToggle(500);
 
 	// Toggles the class (obviously)
 	$appTo.toggleClass('navIsVisible');
-});
+}).preventDefault();
